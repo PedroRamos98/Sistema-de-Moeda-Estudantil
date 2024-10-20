0f912b78a85f881b1223c268ae0d51b2e34b7ff4
DaviAguilar <daviaguilar10@hotmail.com>
2024-10-20 19:42:48 -0300

    Sprint 2 Fix BD

--- code/src/main/java/org/example/moedaestudantecombd/model/Empresa.java
+++ code/src/main/java/org/example/moedaestudantecombd/model/Empresa.java
@@ -1,4 +1,38 @@
 package org.example.moedaestudantecombd.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    // Default constructor
+    public Empresa() {
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
