e413885fe157580ae290e0c4f309b0c954e2da9f
augustoandrade9 <67920085+augustoandrade9@users.noreply.github.com>
2024-11-03 18:23:37 -0300

    Add files via upload

---
+++ code/src/main/java/org/example/moedaestudantecombd/repository/ProfessorRepository.java
@@ -0,0 +1,7 @@
+package org.example.moedaestudantecombd.repository;
+
+import org.example.moedaestudantecombd.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
